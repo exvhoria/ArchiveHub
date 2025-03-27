4313cb875a34dd6e74438b16bc4e42620e86a683
GhosttedXV <162435893+exvhoria@users.noreply.github.com>
2025-03-27 12:16:20 +0700

    Update ArchiveHub.lua

--- src/ArchiveHub.lua
+++ src/ArchiveHub.lua
@@ -1,11 +1,10 @@
 local ESP = loadstring(game:HttpGet("https://raw.githubusercontent.com/exvhoria/ArchiveHub/refs/heads/main/src/ESP.lua"))()
 
--- Configure
-ESP.Config.Box.Scale = 0.7 -- Smaller boxes
-ESP.Config.HealthBar.Position = "Right"
-ESP.Config.ESP.FontSize = 14
+-- Configure settings:
+ESP.Settings.TeamCheck = true
+ESP.Properties.Box.Thickness = 2
+ESP.Properties.HealthBar.Position = 3 -- Left side
 
--- Toggle features
+-- Toggle features:
 ESP:Toggle(true) -- Master switch
 ESP:SetRainbow(true, 0.3) -- Rainbow mode (slower speed)
-ESP:ToggleFeature("Tracer", false) -- Disable tracers
